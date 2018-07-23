14ad2987bf8d37531b1338a41da56d6ce46e1a48
hinsxd <hins.xd@hotmail.com>
2018-07-24 04:04:53 +0900

    Add expense summary to dashboard

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,7 +4,7 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import Header from '../components/Header';
-import HelpPage from '../components/HelpPage';
+// import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 const AppRouter = () => (
   <BrowserRouter>
@@ -15,7 +15,7 @@ const AppRouter = () => (
         <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
         <Route path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
+        {/* <Route path="/help" component={HelpPage} /> */}
         <Route component={NotFoundPage} />
       </Switch>
     </div>
